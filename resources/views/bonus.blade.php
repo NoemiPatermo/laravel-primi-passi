bcaa6fdfdd54ffde737dcb75ee3aff47f5de3eca
noemi86-cpu <noemipatermo@gmail.com>
2021-09-01 19:29:42 +0200

    first exercise and bonus attempt

---
+++ resources/views/bonus.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <style>
+            .menu{
+                margin-top: 20px;
+                display:flex;
+                justify-content: center;
+
+            }
+            a{
+                text-decoration: none;
+                color: RGB(16, 27, 43);
+                padding: 10px
+            }
+        </style>
+        <div class="menu">
+            <a href="https://laravel.com/docs">Oggi</a>
+            <a href="https://laracasts.com">Ho</a>
+            <a href="https://laravel-news.com">Scoperto</a>
+            <a href="https://blog.laravel.com">Laravel!</a>
+            
+           
+        </div>
+    </header>
+</body>
+</html>
